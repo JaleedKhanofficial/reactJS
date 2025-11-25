693991dd3e92620bd0700c3dbd92ec1fd15f562c
Jaleed Khan <jaleedkhan53@gmail.com>
2025-11-25 20:29:19 +0500

    created CREATE Form for user

--- src/assets/pages/Create.jsx
+++ src/assets/pages/Create.jsx
@@ -1,7 +1,49 @@
-export default function Create(){
+import { useState } from "react"
+import http from "../../../http"
+import { useNavigate } from "react-router-dom";
+
+export default function Create() {
+    const vavigate = useNavigate();
+    const [inputs, setInputs] = useState({});
+    
+    const handleChange = (event) =>{
+        const name = event.target.name;
+        const value = event.target.value;
+        setInputs(values => ({...values,[name]:value}))
+    }
+
+    const submitForm = () =>{
+        http.post('/users',inputs).then((res)=>{
+             vavigate('/');
+        })
+    }
+
     return (
-        <di>
-            This is Create us page
-        </di>
+        <div>
+            <h3>New User</h3>
+            <div className="row">
+                <div className="col-sm-6 justify-content-center">
+                    <label>Enter Name</label>
+                    <input type="text" name='name' className="form-control mb-2" 
+                    value={inputs.name || ''}
+                    onChange={handleChange}
+                    />
+
+                    <label>Enter Email</label>
+                    <input type='email' name='email' className="form-control mb-2" 
+                    value={inputs.email || ''}
+                    onChange={handleChange}
+                    />
+                    
+                    <label>Enter Password</label>
+                    <input type='password' name='password' className="form-control mb-2" 
+                    value={inputs.password || ''}
+                    onChange={handleChange}
+                    />
+
+                    <button type="button" onClick={submitForm} className="btn btn-info">Create</button>
+                </div>
+            </div>
+        </div>
     )
 }
